6b20b3a4a5fd4159b461e38351161c1549902503
anarraidoo <anusha.narraidoo@gmail.com>
2024-04-15 10:38:49 -0400

    Update cart service
    Update cart service to access api server

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IProduct } from './catalog/product.model';
 
@@ -7,10 +8,12 @@ import { IProduct } from './catalog/product.model';
 export class CartService {
   cart: IProduct[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   add(product: IProduct) {
     this.cart.push(product);
-    console.log(`product ${ product.name } added to cart`);
+    this.http.post('/api/cart', this.cart).subscribe(() => {
+      console.log(`product ${ product.name } added to cart`);
+    });
   }
 }
